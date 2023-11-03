9c44329d6c5de54d057dbf7092126fec64f7d62c
Danny Ryan <dannykryan@gmail.com>
2023-11-03 12:21:08 +0000

    updated seed data

--- database/scripts/db-reset.js
+++ database/scripts/db-reset.js
@@ -12,6 +12,16 @@ async function resetDatabase() {
     note VARCHAR(250) NOT NULL);`
     );
     //seed database
+    await pool.query(`
+    INSERT INTO notes
+    (title, note)
+    VALUES ("shopping list", "fish, eggs, bananas, catnip"),
+    ("Meeting with Mr. Smith", "Tuesday at 9. Bring an apple"),
+    ("Reminders", "pay bill, wash car, wash cat"),
+    ("Films to watch", "Titanic, Pet Semetary, Death Becomes Her"),
+    ("Random", "Wikipedia Oprah Winfrey and find out what's up with her"),
+    ("Ideas for film series", "Monkey Tennis - what it sounds like. Monkeys playing tennis. Supertrains and the people that made them with Michael Portillo")
+    `);
   } catch (error) {
     console.error("Database failed to reset: ", error);
   } finally {
