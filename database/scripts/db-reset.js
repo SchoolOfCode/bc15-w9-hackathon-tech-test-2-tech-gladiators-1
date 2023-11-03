398da0420dde287f73a2338b6529d777bbd54dc8
Danny Ryan <dannykryan@gmail.com>
2023-11-03 12:13:34 +0000

    updated schema

--- database/scripts/db-reset.js
+++ database/scripts/db-reset.js
@@ -1,24 +1,23 @@
-import {pool} from "../index.js"
+import { pool } from "../index.js";
 
-async function resetDatabase(){
-try{
-//delete database if exists
-await pool.query(`DROP TABLE IF EXISTS notes CASCADE`);
-//create database
-await pool.query(
-    `CREATE TABLE notes (
+async function resetDatabase() {
+  try {
+    //delete database if exists
+    await pool.query(`DROP TABLE IF EXISTS notes CASCADE`);
+    //create database
+    await pool.query(
+      `CREATE TABLE notes (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    title VARCHAR(250) NOT NULL,
-    note VARCHAR NOT NULL);`);
-//seed database
-
-} catch(error) {
-
-    console.error("Database failed to reset: ". error)
-
-} finally {
+    title VARCHAR(50) NOT NULL,
+    note VARCHAR(250) NOT NULL);`
+    );
+    //seed database
+  } catch (error) {
+    console.error("Database failed to reset: ", error);
+  } finally {
+    // closes the pool to the database
     await pool.end();
-}
+  }
 }
 
-await resetDatabase()
+await resetDatabase();
